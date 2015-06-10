0db72b9feb36e2622837728b27c1943ab96eaf64
Morten Bartvig <bartvig@gmail.com>
2015-06-10 10:53:57 +0200

    Added git to provision.

--- provision.sh
+++ provision.sh
@@ -13,6 +13,8 @@ sudo apt-get -y install php5-mysql mysql-server mysql-client php5-curl
 sudo apt-get -y install nodejs npm nodejs-legacy
 sudo apt-get -y install ruby-full
 
+sudo apt-get -y install git
+
 sudo gem install sass
 sudo gem install compass
 sudo gem install bootstrap-sass
